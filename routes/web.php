f7499252332bed54a1740305129254cca8548bff
Leah Chan <lec533@g.harvard.edu>
2016-10-22 13:15:06 -0400

    updated default route and created inline html for landing page

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,17 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    $msg = '<h1>You are on the landing page.</h1></br>';
+
+    $msg .= '<h2>TO DOs</h2></br>';
+    $msg .= '[ ] Create blurb for ipsum generator</br>';
+    $msg .= '[ ] Create blurb for random user generator</br>';
+    $msg .= '[ ] Create corresponding link to each tool</br>';
+
+    $msg .= '[ ] Create page for ipsum generator</br>';
+    $msg .= '[ ] Create page for random user generator</br>';
+
+    return $msg;
+
 });
