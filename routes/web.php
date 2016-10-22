1612d4eb1d94314e1ca39fa70cb37b6592322cf1
Leah Chan <lec533@g.harvard.edu>
2016-10-22 13:54:12 -0400

    added routes for ipsum and random user generator

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,30 @@ Route::get('/', function () {
     return $msg;
 
 });
+
+Route::get('/generate_ipsum', function () {
+    //return view('welcome');
+    $msg = '<h1>Ipsum Generator</h1></br>';
+
+    $msg .= '<h2>TO DOs</h2></br>';
+    $msg .= '[ ] Create blurb for ipsum generator</br>';
+    $msg .= '[ ] Create form options for ipsum generator</br>';
+    $msg .= '[ ] Determine how to display results from ipsum generator</br>';
+    $msg .= '[ ] Install ipsum generator package</br>';
+
+    return $msg;
+});
+
+
+Route::get('/generate_users', function () {
+    //return view('welcome');
+    $msg = '<h1>Random Users Generator</h1></br>';
+
+    $msg .= '<h2>TO DOs</h2></br>';
+    $msg .= '[ ] Create blurb for random user generator</br>';
+    $msg .= '[ ] Create form options for random user generator</br>';
+    $msg .= '[ ] Determine how to display results from random user generator</br>';
+    $msg .= '[ ] Install random user package</br>';
+
+    return $msg;
+});
