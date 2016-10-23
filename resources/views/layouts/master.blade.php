5a6d9e17c8ddf2d31436a01d44a8a9db71d27b91
Leah Chan <lec533@g.harvard.edu>
2016-10-22 22:45:53 -0400

    added normalize css CDN

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,28 +10,32 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
     integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+    <link rel='stylesheet' href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css" >
+
+
     {{-- Yield any page specific CSS files or anything else you might want in the <head> --}}
     @yield('head')
 
 </head>
 <body>
+  <div class="container">
 
-    <header>
+      <header>
 
-    </header>
+      </header>
 
-    <section>
-        {{-- Main page content will be yielded here --}}
-        @yield('content')
-    </section>
+      <section>
+          {{-- Main page content will be yielded here --}}
+          @yield('content')
+      </section>
 
-    <footer>
-    </footer>
+      <footer>
+      </footer>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
-    {{-- Yield any page specific JS files or anything else you might want at the end of the body --}}
-    @yield('body')
-
+      {{-- Yield any page specific JS files or anything else you might want at the end of the body --}}
+      @yield('body')
+  </div>
 </body>
 </html>
