a0696e8bab5cfd91aeba7553933d18beaa07a606
Leah Chan <lec533@g.harvard.edu>
2016-10-22 20:26:27 -0400

    code clean up, removed inline html, updated controllers in Routes, updated Route names

--- routes/web.php
+++ routes/web.php
@@ -11,85 +11,19 @@
 |
 */
 
-# Direct user to the landing page introducing both tools using IpsumController
-Route::get('/', 'IpsumController@index')->name('generator.index');
-
-#OLD
-# Directs user to the landing page introducing both tools
-/*
-Route::get('/', function () {
-    //return view('welcome');
-    $msg = '<h1>You are on the landing page.</h1></br>';
-
-    $msg .= '<h2>TO DOs</h2></br>';
-    $msg .= '[ ] Create blurb for ipsum generator</br>';
-    $msg .= '[ ] Create blurb for random user generator</br>';
-    $msg .= '[ ] Create corresponding link to each tool</br>';
-
-    $msg .= '[ ] Create page for ipsum generator</br>';
-    $msg .= '[ ] Create page for random user generator</br>';
-
-    return $msg;
-
-});
-*/
+# Direct user to the Generator tools landing page using IpsumController
+Route::get('/', 'IndexController@index')->name('gen_home.index');
 
 # Direct user to the ipsum generator landing page using IpsumController
 Route::get('/generate_ipsum', 'IpsumController@index')->name('gen_ipsum.index');
 
-#OLD
-# Directs user to the landing page of the random ipsum generator
-/*
-Route::get('/generate_ipsum', function () {
-    //return view('welcome');
-    $msg = '<h1>Ipsum Generator</h1></br>';
-
-    $msg .= '<h2>TO DOs</h2></br>';
-    $msg .= '[ ] Create blurb for ipsum generator</br>';
-    $msg .= '[ ] Create form options for ipsum generator</br>';
-    $msg .= '[ ] Determine how to display results from ipsum generator</br>';
-    $msg .= '[ ] Install ipsum generator package</br></br>';
-
-    $view = '<form method = "POST" action = "/generate_ipsum">';
-    $view .= csrf_field(); # This will be explained more later
-    $view .=  '<label>Number of Paragraphs: <input type = "text" name ="num paragrphs">';
-    $view .=  '<input type = "submit">';
-    $view .= '</form>';
-
-    return $msg . $view;
-});
-*/
-
-# Directs user to the confirmation page of the random user generator
+# Directs user to the confirmation page of the ipsum generator
 Route::post('/generate_ipsum', function () {
     return 'Ipsum generated.';
 });
 
 # Direct user to the user generator landing page using IpsumController
 Route::get('/generate_users', 'UserController@index')->name('gen_user.index');
-
-#OLD
-# Directs user to the landing page of the random user generator
-/*
-Route::get('/generate_users', function () {
-    //return view('welcome');
-    $msg = '<h1>Random Users Generator</h1></br>';
-
-    $msg .= '<h2>TO DOs</h2></br>';
-    $msg .= '[ ] Create blurb for random user generator</br>';
-    $msg .= '[ ] Create form options for random user generator</br>';
-    $msg .= '[ ] Determine how to display results from random user generator</br>';
-    $msg .= '[ ] Install random user package</br></br>';
-
-    $view = '<form method = "POST" action = "/generate_users">';
-    $view .= csrf_field(); # This will be explained more later
-    $view .=  '<label>Number of Users: <input type = "text" name ="num users">';
-    $view .=  '<input type = "submit">';
-    $view .= '</form>';
-
-    return $msg . $view;
-});
-*/
 
 # Directs user to the confirmation page of the random user generator
 Route::post('/generate_users', function () {
