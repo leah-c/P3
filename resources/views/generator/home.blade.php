a1eaf1534246e53f4f23cd43654e420870058b73
Leah Chan <lec533@g.harvard.edu>
2016-10-22 21:41:22 -0400

    modified content

--- resources/views/generator/home.blade.php
+++ resources/views/generator/home.blade.php
@@ -18,30 +18,64 @@ such as a page specific stylesheets.
 
 @section('content')
 
-  <h1>You are on the Generator landing page.</h1>
+  <h1>Developer's Best Friend</h1>
+  <p>Look no further if you're looking for text to fill up blank space or need test user data!</p>
+
+  <h2>Lorem Ipsum Text Generator</h2>
+  <p>The Lorem Ipsum Text Generator tool will give you some dummy text. Just enter the
+  number of paragraphs of text you'd like to have!</p>
+
+  <h2>Random user Generator</h2>
+  <p>The Random User Generator tool will give you some dummy user names (i.e. Lisa Frank).</p>
 
   <ul><h2>TO DOs</h2>
     <li>
       <ul><h1>Ipsum Generator</h1>
-        <li>[ ] Create blurb for ipsum generator</li>
+        <li>[ x ] Create blurb for ipsum generator</li>
         <li>[ ] Determine how to display results from ipsum generator </li>
-        <li>[ ] Create link to ipsum generator </li>
-        <li>[ ] Install ipsum generator package </li>
+        <li>[ ] Create link to ipsum generator from home page</li>
+        <li>[ x ] Install ipsum generator package </li>
       </ul>
     </li>
 
     <li>
       <ul><h1>User Generator</h1>
-        <li>[ ] Create link to user generator </li>
+        <li>[ x ] Create blurb for random user generator</li>
+        <li>[ ] Create link to user generator from home page</li>
         <li>[ ] Determine how to display results from user generator </li>
         <li>[ ] Install random user generator package </li>
       </ul>
     </li>
 
     <li>
+      <ul><h1>Validation/ Error Messages</h1>
+        <li>[ ] Test link to user generator </li>
+        <li>[ ] Create Laravel Validation rules for user generator </li>
+        <li>[ ] Test random user generator package </li>
+
+        <li>[ ] Test link to ipsum generator </li>
+        <li>[ ] Create Laravel Validation rules for ipsum generator </li>
+        <li>[ ] Test ipsum generator package </li>
+      </ul>
+    </li>
+
+    <li>
+      <ul><h1>Structure</h1>
+        <li>[ ] Create form for ipsum generator </li>
+        <li>[ ] Create form for user generator </li>
+      </ul>
+    </li>
+
+    <li>
+      <ul><h1>Aesthetics</h1>
+        <li>[ ] Make forms look pretty </li>
+      </ul>
+    </li>
+
+    <li>
       <ul><h1>Extras</h1>
         <li>[ ] Incorporate password generator from P2 </li>
-        <li>[ ] Create link to password generator </li>
+        <li>[ ] Create link to password generator on home page</li>
         <li>[ ] Determine how to display results from password generator </li>
       </ul>
     </li>
