6f8607245168593c200c3a20804b1f0ab99d907a
Leah Chan <lec533@g.harvard.edu>
2016-10-22 22:22:55 -0400

    commented out test Routes

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,22 @@
 */
 
 # Direct user to the Generator tools landing page using IpsumController
-#Route::get('/', 'IndexController@index')->name('gen_home.index');
 Route::get('/', 'GeneratorIndexController@index')->name('gen_home.index');
+
+/*
+Route::get('/ipsum_test', function(){
+  $generator = new Badcow\LoremIpsum\Generator();
+  $paragraphs = $generator->getParagraphs(5);
+  echo implode('<p>', $paragraphs);
+});
+
+Route::get('/user_test', function(){
+  $faker = Faker\Factory::create();
+  for ($i=0; $i < 10; $i++) {
+  echo $faker->name, "\n";
+}
+});
+*/
 
 # Direct user to the ipsum generator landing page using IpsumController
 Route::get('/generate_ipsum', 'IpsumController@index')->name('gen_ipsum.index');
