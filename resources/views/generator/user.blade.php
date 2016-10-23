912af6e79afb7dfc08b64815fc2708120dd30ed5
Leah Chan <lec533@g.harvard.edu>
2016-10-22 22:29:18 -0400

    updated CSS references, added Boostrap CDN to master template

--- resources/views/generator/user.blade.php
+++ resources/views/generator/user.blade.php
@@ -12,7 +12,7 @@ Use it to add specific things that *this* View needs in the head,
 such as a page specific stylesheets.
 --}}
 @section('head')
-    <link href="/css/books/show.css" type='text/css' rel='stylesheet'>
+    {{--<link href="/css/books/show.css" type='text/css' rel='stylesheet'>--}}
 @stop
 
 
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,8 @@
     </title>
 
     <meta charset='utf-8'>
-    <link href="/css/foobooks.css" type='text/css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
     {{-- Yield any page specific CSS files or anything else you might want in the <head> --}}
     @yield('head')
@@ -16,12 +17,7 @@
 <body>
 
     <header>
-        {{--
-          <img
-        src='http://making-the-internet.s3.amazonaws.com/laravel-foobooks-logo@2x.png'
-        style='width:300px'
-        alt='Foobooks Logo'>
-        --}}
+
     </header>
 
     <section>
