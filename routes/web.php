3010d86b25c8f265ab011d6cb01175ad3e0f650d
Leah Chan <lec533@g.harvard.edu>
2016-10-26 20:08:33 -0400

    updated controller reference and method calls

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,19 @@
 |
 */
 
+Route::get('/', 'DEVBFF@home')->name('gen_home.index');
+
+Route::get('/generate_ipsum', 'DEVBFF@ipsumHome')->name('gen_ipsum.index');
+
+Route::get('/generate_users', 'DEVBFF@userHome')->name('gen_user.index');
+
+Route::post('/generate_ipsum', 'DEVBFF@createIpsum')->name('gen_ipsum.create');
+
+Route::post('/generate_users', 'DEVBFF@createUsers')->name('gen_user.create');
+
+
+
+/*
 # Direct user to the Generator tools landing page using IpsumController
 Route::get('/', 'GeneratorIndexController@index')->name('gen_home.index');
 
@@ -25,3 +38,4 @@ Route::get('/generate_users', 'UserController@index')->name('gen_user.index');
 
 # Directs user to the confirmation page of the random user generator
 Route::post('/generate_users', 'UserController@create')->name('gen_user.create');
+*/
