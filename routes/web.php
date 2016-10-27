09c2bbfc51c426979e2b3fbfa10784a66680c018
Leah Chan <lec533@g.harvard.edu>
2016-10-26 20:41:18 -0400

    code tidy

--- routes/web.php
+++ routes/web.php
@@ -11,31 +11,17 @@
 |
 */
 
+# Direct user to the Generator tools landing page
 Route::get('/', 'DEVBFF@home')->name('gen_home.index');
 
+# Direct user to the ipsum text generator landing page
 Route::get('/generate_ipsum', 'DEVBFF@ipsumHome')->name('gen_ipsum.index');
 
+# Direct user to the user generator landing page
 Route::get('/generate_users', 'DEVBFF@userHome')->name('gen_user.index');
 
+# Directs user to the confirmation page of the ipsum generator
 Route::post('/generate_ipsum', 'DEVBFF@createIpsum')->name('gen_ipsum.create');
 
-Route::post('/generate_users', 'DEVBFF@createUsers')->name('gen_user.create');
-
-
-
-/*
-# Direct user to the Generator tools landing page using IpsumController
-Route::get('/', 'GeneratorIndexController@index')->name('gen_home.index');
-
-# Direct user to the ipsum generator landing page using IpsumController
-Route::get('/generate_ipsum', 'IpsumController@index')->name('gen_ipsum.index');
-
-# Directs user to the confirmation page of the ipsum generator
-Route::post('/generate_ipsum', 'IpsumController@create')->name('gen_ipsum.create');
-
-# Direct user to the user generator landing page using IpsumController
-Route::get('/generate_users', 'UserController@index')->name('gen_user.index');
-
 # Directs user to the confirmation page of the random user generator
-Route::post('/generate_users', 'UserController@create')->name('gen_user.create');
-*/
+Route::post('/generate_users', 'DEVBFF@createUsers')->name('gen_user.create');
